1502b668cb1e3b2c458a3ef8bfe072bd2eec1165
Lim Whei Han <han9.2000@gmail.com>
2023-03-17 00:10:21 +0800

    Updates

---
+++ precache-manifest.dbc30fd66013066b5b4d2e19ffc9929e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "50f455ed83e4e8af221e",
+    "url": "/reactresume/static/css/main.5b1ba1d4.chunk.css"
+  },
+  {
+    "revision": "50f455ed83e4e8af221e",
+    "url": "/reactresume/static/js/main.50f455ed.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/reactresume/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "425014b05a89adfbde06",
+    "url": "/reactresume/static/js/2.425014b0.chunk.js"
+  },
+  {
+    "revision": "9632561f1bae3f99239b",
+    "url": "/reactresume/static/js/runtime~main.9632561f.js"
+  },
+  {
+    "revision": "c2010865f9ce16622978fd1a6e033d16",
+    "url": "/reactresume/static/media/pfp.c2010865.jpg"
+  },
+  {
+    "revision": "ad6d904660d401d528510c800413f6cf",
+    "url": "/reactresume/index.html"
+  }
+];
